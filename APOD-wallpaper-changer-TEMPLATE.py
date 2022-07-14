2e3eb50fd35d55e55744390172614acaa5d601fa
yetiboy <yetiboy@gmail.com>
2022-07-14 16:05:40 -0400

    Add files via upload

---
+++ APOD-wallpaper-changer-TEMPLATE.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+import time
+import json
+import requests
+import re
+import struct
+import ctypes
+
+apirawdata = requests.get("https://api.nasa.gov/planetary/apod?api_key=<INSERT YOUR API KEY HERE>&hd=true")  #NOTE: REPLACE <INSERT YOUR API KEY HERE> WITH YOUR API KEY (DON'T INCLUDE <>)
+
+apidata = apirawdata.json()
+
+hdurl = apidata['hdurl']
+
+image = requests.get(hdurl, allow_redirects=True)
+
+open ('background.jpg', 'wb').write(image.content)
+
+time.sleep(300)
+
+PATH = 'C:\\PATH\\TO\\BACKGROUND\\IMAGE\\background.jpg' # NOTE: USE DOUBLE BACKSLASHES!
+SPI_SETDESKWALLPAPER = 20
+
+ctypes.windll.user32.SystemParametersInfoW(SPI_SETDESKWALLPAPER, 0, PATH, 3)
